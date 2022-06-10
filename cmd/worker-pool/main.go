a38f03d45aecefdab18bf1ac5772ad36737f20e7
Vladimir Sokolov <vsvastey@gmail.com>
2022-06-10 23:05:28 +0300

    Add simple main

--- cmd/worker-pool/main.go
+++ cmd/worker-pool/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Vastey/worker-pool/internal/progressbar"
+	"github.com/Vastey/worker-pool/internal/task"
+	"time"
+)
+
 func main() {
-	return
+	pb := progressbar.NewProgressBar("task1:")
+	t := task.NewSleepTask(10*time.Second)
+	ch := t.Do()
+	for status := range ch{
+		pb.Set(status.Progress)
+		fmt.Printf("\r%s", pb.Draw())
+	}
 }
