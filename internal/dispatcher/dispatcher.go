447dac7fde113e0387ec33891050e6620f59aba3
Vladimir Sokolov <vsvastey@gmail.com>
2022-06-13 00:50:32 +0300

    Simplify Dispatcher

--- internal/dispatcher/dispatcher.go
+++ internal/dispatcher/dispatcher.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Dispatcher struct {
-	tasks chan task.Task
+	tasks        chan task.Task
 	workersInput chan chan task.Task
 }
 
-func NewDispatcher(workers []worker.Worker, tasks chan task.Task, wg *sync.WaitGroup) *Dispatcher{
+func NewDispatcher(workers []worker.Worker, tasks chan task.Task, wg *sync.WaitGroup) *Dispatcher {
 	workersInput := make(chan chan task.Task, len(workers))
 	for _, w := range workers {
 		go func(worker worker.Worker) {
@@ -19,19 +19,16 @@ func NewDispatcher(workers []worker.Worker, tasks chan task.Task, wg *sync.WaitG
 		}(w)
 	}
 	return &Dispatcher{
-		tasks:   tasks,
+		tasks:        tasks,
 		workersInput: workersInput,
 	}
 }
 
 func (d *Dispatcher) Dispatch() {
-	for {
-		select {
-		case t := <-d.tasks:
-			go func(t task.Task) {
-				ch := <-d.workersInput
-				ch <- t
-			}(t)
-		}
+	for t := range d.tasks {
+		go func(t task.Task) {
+			ch := <-d.workersInput
+			ch <- t
+		}(t)
 	}
 }
